25bf916c749d6145a70d5a058fa9c12c4373b146
TomVilla <fvillanuevanut@gmail.com>
2020-10-06 23:59:06 -0500

    lab3 fisica

---
+++ lab3_Newton/new1_1.py
@@ -0,0 +1,82 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+h=0.01
+a=2
+r=8
+
+x=r
+y=0
+
+vx=0
+vy=4
+
+px=[]
+py=[]
+pv=[]
+pa=[]
+pt = np.arange(0,13,h)
+
+for t in pt:
+	ax= -(a*x/r)
+	ay= -(a*y/r)
+
+	x = x + vx * h
+	y = y + vy * h
+
+	vx = vx + ax * h
+	vy = vy + ay * h
+
+	px.append(x)
+	py.append(y)
+	
+	pv.append([vx,vy])
+	pa.append([ax,ay])
+	
+
+#print("(a) posicion final: ",px[len(px)-1])
+#print("(b) posicion final: ",px2[len(px2)-1])
+#print("(c) posicion final: ",px3[len(px3)-1])
+
+plt.subplot(2,2,1)
+plt.plot(pt,px)
+plt.xlabel('tiempo')
+plt.ylabel('espacio')
+plt.title('x-t')
+plt.grid(True)
+
+plt.subplot(2,2,2)
+plt.plot(pt,pv)
+plt.xlabel('tiempo')
+plt.ylabel('velocidad')
+plt.title('v-t')
+plt.grid(True)
+
+plt.subplot(2,2,3)
+plt.plot(pt,pa)
+plt.xlabel('tiempo')
+plt.ylabel('aceleracion')
+plt.title('a-t')
+plt.grid(True)
+
+plt.subplot(2,2,4)
+plt.plot(px,py)
+plt.xlabel('espacio')
+plt.ylabel('velocidad')
+plt.title('espacio de fases')
+plt.grid(True)
+
+plt.show()
+
+axs=plt.subplot()
+axs.set_aspect('equal')
+plt.plot(px,py)
+plt.xlabel('X axis')
+plt.xlabel('Y axis')
+plt.title('X-Y')
+plt.grid(True)
+plt.show()
+
+rsim = math.sqrt(x*x + y*y)
+print('R simulada:' ,rsim)
+print('error: :' ,abs(8-rsim)/8*1)
---
+++ lab3_Newton/new2_1.py
@@ -0,0 +1,105 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+h=0.01
+
+v=[50,50]
+r=0.0367
+m=0.145
+c=0.8
+rho=1.2
+Area = np.pi * (r**2)
+k=(c*Area*rho)/(2*m)
+print(k)
+
+ball = [[0,0],[0,0]]
+vBall = [[v[0]*np.cos(np.pi/3) , v[0] * np.sin(np.pi/3)], [v[1]*np.cos(np.pi/3) , v[1] * np.sin(np.pi/3)]]
+#vxa = v * np.cos(np.pi/3)
+#vya = v * np.sin(np.pi/3)
+aBall = [[0,-10],[0,-10]]
+
+#axa=0
+#aya=-10
+
+
+pxBall=[[]for i in range(len(ball))]
+pyBall=[[]for i in range(len(ball))]
+
+axs=plt.subplot()
+axs.set_aspect('equal')
+
+pt = np.arange(0,20,h)
+for it in range(len(ball)):
+	for t in pt:
+		if(it==0):
+			v[it] = np.sqrt(vBall[it][0]**2+vBall[it][1]**2)
+			aBall[it][0] = -k*v[it]*vBall[it][0]
+			aBall[it][1] = -10-k*v[it]*vBall[it][1]
+
+		ball[it][0] = ball[it][0] + vBall[it][0] * h
+		ball[it][1] = ball[it][1] + vBall[it][1] * h
+
+		vBall[it][0] = vBall[it][0] + aBall[it][0] * h
+		vBall[it][1] = vBall[it][1] + aBall[it][1] * h
+		
+		pxBall[it].append(ball[it][0])
+		pyBall[it].append(ball[it][1])
+		if(ball[it][1] <= 0):
+			break
+		
+plt.plot(pxBall[0],pyBall[0])
+plt.plot(pxBall[1],pyBall[1])
+
+plt.xlabel('X axis')
+plt.xlabel('Y axis')
+plt.title('X-Y')
+plt.grid(True)
+plt.show()
+"""
+#print("(a) posicion final: ",px[len(px)-1])
+#print("(b) posicion final: ",px2[len(px2)-1])
+#print("(c) posicion final: ",px3[len(px3)-1])
+
+plt.subplot(2,2,1)
+plt.plot(pt,px)
+plt.xlabel('tiempo')
+plt.ylabel('espacio')
+plt.title('x-t')
+plt.grid(True)
+
+plt.subplot(2,2,2)
+plt.plot(pt,pv)
+plt.xlabel('tiempo')
+plt.ylabel('velocidad')
+plt.title('v-t')
+plt.grid(True)
+
+plt.subplot(2,2,3)
+plt.plot(pt,pa)
+plt.xlabel('tiempo')
+plt.ylabel('aceleracion')
+plt.title('a-t')
+plt.grid(True)
+
+plt.subplot(2,2,4)
+plt.plot(px,py)
+plt.xlabel('espacio')
+plt.ylabel('velocidad')
+plt.title('espacio de fases')
+plt.grid(True)
+
+plt.show()
+
+axs=plt.subplot()
+axs.set_aspect('equal')
+plt.plot(px,py)
+plt.xlabel('X axis')
+plt.xlabel('Y axis')
+plt.title('X-Y')
+plt.grid(True)
+plt.show()
+
+rsim = math.sqrt(x*x + y*y)
+print('R simulada:' ,rsim)
+print('error: :' ,abs(8-rsim)/8*1)
+"""
